2ad4ee1014d14d9df3a06e5d283bfa38fd43e596
shubham1166 <49393148+shubham1166@users.noreply.github.com>
2020-12-09 23:46:18 +0530

    Update network.py

--- network.py
+++ network.py
@@ -7,18 +7,18 @@ class AutoEncoder(nn.Module):
         super(AutoEncoder, self).__init__()
 
         self.encoder = nn.Sequential(
-            nn.Linear(input_size, int(input_size/10)),
+            nn.Linear(input_size, int(input_size/2)),
             nn.Sigmoid(),
-            nn.Linear(int(input_size/10), int(input_size/100)),
+            nn.Linear(int(input_size/2), int(input_size/4)),
             nn.Sigmoid(),
-            nn.Linear(int(input_size/100), int(input_size/50)),  
+            nn.Linear(int(input_size/4), int(input_size/8)),  
         )
         self.decoder = nn.Sequential(
-            nn.Linear(int(input_size/50), int(input_size/100)),
+            nn.Linear(int(input_size/8), int(input_size/4)),
             nn.Sigmoid(),
-            nn.Linear(int(input_size/100), int(input_size/10)),
+            nn.Linear(int(input_size/4), int(input_size/2)),
             nn.Sigmoid(),
-            nn.Linear(int(input_size/10), input_size),
+            nn.Linear(int(input_size/2), input_size),
             nn.Sigmoid(),  
         )
 
@@ -30,11 +30,14 @@ class AutoEncoder(nn.Module):
     
 class Graph_embedding(nn.Module):
     def __init__(self, embedding_size, output_size):
-        super(AutoEncoder, self).__init__()
-
-        self.embedding = nn.Linear(output_size, embedding_size)
+        super(Graph_embedding, self).__init__()
+        
+        self.embedding = nn.Sequential(
+            nn.Sigmoid(),
+            nn.Linear(output_size, embedding_size),
+            nn.Sigmoid(),
+        )
 
     def forward(self, x):
         embd = self.embedding(x)
         return embd
-    
